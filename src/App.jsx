fccda4b58b5bcd3768abfc23df201f76d854402b
shan-ti <thoselovelywords@gmail.com>
2025-07-14 13:14:16 +0900

    created a new component folder for Movie App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import Eigakan from "./components/eigakan"
-import PizzaMaker from "./components/PizzaMaker"
 
+import Eigakan from "./components/Eigakan/Eigakan";
+import PizzaMaker from "./components/PizzaMaker";
 
 function App() {
-
   return (
     <>
-      <div className='bg-slate-950 mx-auto text-center py-4'>
-        <h1 className="text-slate-300 text-6xl">Journey to Fullstack Projects</h1>
+      <div className="bg-slate-950 mx-auto text-center py-4">
+        <h1 className="text-slate-300 text-6xl">
+          Journey to Fullstack Projects
+        </h1>
         <Eigakan />
         <PizzaMaker />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Eigakan/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navbar = () => {
+
+  return (
+    <>
+      <nav className="flex justify-between items-center bg-slate-900 h-12 px-4 py-6 rounded-md mt-4">
+        <div>
+          <h1 className="text-xl font-bold text-slate-300">
+            <span>Eiga</span>
+            <span className="text-slate-500">kan</span>
+          </h1>
+        </div>
+        <input
+          type="text"
+          className="bg-slate-800 text-slate-100 placeholder-slate-400 rounded-md px-4 py-1 focus:outline-none focus:ring-2 focus:ring-slate-500"
+          placeholder="search movies..."
+        />
+        <div className="text-slate-300 text-md">
+          <p>
+            <strong>Found</strong> results
+          </p>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
